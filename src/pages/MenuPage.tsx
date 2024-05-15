2c17a746870bc596d2d702cfc46002f89f7106b2
DefEnge <damiano.coppola03@gmail.com>
2024-05-15 17:38:11 +0200

    refactor: modified and moved MenuPage

--- src/pages/MenuPage.tsx
+++
@@ -1,7 +0,0 @@
-import { Button } from "../components"
-
-const MenuPage = () => (
-  <div> <Button> </Button> </div>
-
-)
-export default MenuPage
---
+++ src/pages/MenuPage/MenuPage.tsx
@@ -0,0 +1,13 @@
+import { Wrapper, NavBar, MuiButton } from "../../components";
+
+
+const MenuPage: React.FC = () => {
+  return (
+    <Wrapper>
+      <NavBar />
+    </Wrapper>
+
+  );
+
+}
+export default MenuPage
