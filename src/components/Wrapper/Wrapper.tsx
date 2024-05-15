3479497910403dd393d6412ca958b1ad4f8bbf94
DefEnge <damiano.coppola03@gmail.com>
2024-05-15 17:40:17 +0200

    feat: added wrapper component

---
+++ src/components/Wrapper/Wrapper.tsx
@@ -0,0 +1,11 @@
+
+type WrapperProps = {
+  children: React.ReactNode
+}
+
+const Wrapper: React.FC<WrapperProps> = ({ children }) => {
+  return (
+    <div>{children}</div>
+  );
+}
+export default Wrapper
