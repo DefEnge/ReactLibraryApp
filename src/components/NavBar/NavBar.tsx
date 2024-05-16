b1f5e31aa3c4d08f0ab3685ebab89b30cdbc3ac6
DefEnge <damiano.coppola03@gmail.com>
2024-05-16 12:54:13 +0200

    feat: added icon

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,7 +1,13 @@
 import { Drawer, Box, List, ListItemIcon, ListItem, ListItemButton, ListItemText, Divider } from "@mui/material";
+import MenuIcon from '@mui/icons-material/Menu';
+import MenuBookIcon from '@mui/icons-material/MenuBook';
+import FileUploadIcon from '@mui/icons-material/FileUpload';
+import FileDownloadIcon from '@mui/icons-material/FileDownload';
+import PeopleIcon from '@mui/icons-material/People';
 import Wrapper from "../Wrapper/Wrapper";
 import React from "react";
 import { Links, NavLinks } from "../../constants";
+import { BookMenu } from "../../svg";
 import MuiButton from "../Button/Button";
 const NavBar: React.FC = () => {
 
@@ -10,16 +16,32 @@ const NavBar: React.FC = () => {
   const toggleDrawer = (newOpen: boolean) => () => {
     setOpen(newOpen);
   };
+  const iconSelector: React.ElementType = (text: string) => {
+    switch (text) {
+      case 'Menu':
+        return <MenuIcon />;
+      case 'Dashboard':
+        return <MenuBookIcon />;
+      case 'Cedi':
+        return <FileUploadIcon />;
+      case 'Prendi':
+        return <FileDownloadIcon />;
+      case 'Utenti':
+        return <PeopleIcon />;
+      default:
+        return <></>;
+    }
+  };
 
   const DrawerList = (
-    <Box sx={{ width: 250 }} role="presentation" onClick={toggleDrawer(false)} >
+    <Box sx={{ width: 250, }} role="presentation" onClick={toggleDrawer(false)} >
       <List>
         {NavLinks.map((text, index) => (
           <>
             <ListItem key={index} >
               <ListItemButton component="a" href={Links[index]} >
                 <ListItemIcon>
-
+                  {iconSelector(text)}
                 </ListItemIcon>
                 <ListItemText primary={text} />
               </ListItemButton>
@@ -27,15 +49,17 @@ const NavBar: React.FC = () => {
             <Divider component="li" />
           </>
         ))}
+
+        <BookMenu sx={{ width: "15rem", height: "15rem", opacity: "25%", marginTop: "44rem" }} />
       </List>
     </Box>
   );
 
   return (
     <Wrapper>
-      <MuiButton onClick={toggleDrawer(true)} Text="Menu" ></MuiButton>
+      <MuiButton style={{}} onClick={toggleDrawer(true)} Text="Menu" ></MuiButton>
       <Drawer open={open} onClose={toggleDrawer(false)}>{DrawerList}</Drawer>
-    </Wrapper>
+    </Wrapper >
 
   );
 }
