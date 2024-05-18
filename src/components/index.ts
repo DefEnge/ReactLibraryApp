4d50ea94ba968f3788e642c9ea1435c57c9d6cad
DefEnge <damiano.coppola03@gmail.com>
2024-05-19 00:31:38 +0200

    feat: added export for dashboardtable

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,9 +1,10 @@
 import MuiButton from "./Button/Button";
 import NavBar from "./NavBar/NavBar";
 import MenuButton from "./Button/MenuButton";
-
+import DashboardTable from "./Table/DashboardTable";
 export {
   MenuButton,
   MuiButton,
   NavBar,
+  DashboardTable,
 }
