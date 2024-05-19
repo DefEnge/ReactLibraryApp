61a136ea68160cbbff27cb9aeed33117528dcc2a
DefEnge <damiano.coppola03@gmail.com>
2024-05-19 16:51:44 +0200

    feat: added export for popup

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,9 +2,13 @@ import MuiButton from "./Button/Button";
 import NavBar from "./NavBar/NavBar";
 import MenuButton from "./Button/MenuButton";
 import DashboardTable from "./Table/DashboardTable";
+import PopUpDialog from "./Dialog/PopUpDialog";
+import PopUpCancel from "./Dialog/PopupCancel";
 export {
   MenuButton,
   MuiButton,
   NavBar,
   DashboardTable,
+  PopUpDialog,
+  PopUpCancel
 }
